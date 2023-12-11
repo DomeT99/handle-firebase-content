749fa7810126dcb64493940022d4d09f24aab1a5
Domenico Tenace <domenicotenace99@gmail.com>
2023-12-11 23:27:13 +0100

    ref:refactoring navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,21 +2,23 @@
 import { ref } from "vue";
 
 const openMenu = ref<boolean>(false);
+
+function toggleSideBar() {
+  openMenu.value = !openMenu.value;
+}
 </script>
 <template>
   <v-app-bar color="primary" elevation="10">
-    <v-app-bar-nav-icon
-      @click="openMenu = !openMenu"
-      variant="text"
-    ></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="toggleSideBar" variant="text"></v-app-bar-nav-icon>
   </v-app-bar>
+  
   <v-navigation-drawer
-    color="secondary"
+    color="primary"
     elevation="10"
     v-model="openMenu"
     :temporary="true"
   >
-    <v-list density="compact" nav style="margin-top: 1rem">
+    <v-list class="custom-mt-1" density="compact" nav>
       <v-list-item
         prepend-icon="mdi-view-dashboard"
         title="Home"
